68658f1cdf7e70974874f7fbaf94c630c5057677
marcelo covre <marcelocovre@outlook.com>
2023-05-16 16:09:44 -0300

    First request

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,11 @@
 
-console.log('Sucesso!')
+const offset = 0
+const limit = 0
+const url = `https://pokeapi.co/api/v2/pokemon?offSet=${offset}&limit=${limit}`
+
+fetch(url).then(function (response) {
+    console.log(response)
+})
+
+const x = 10 + 10
+console.log(x)
